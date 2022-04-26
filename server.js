962b51a66d96c87735eebef17bee6905298fc685
kouzber123 <48252892+kouzber123@users.noreply.github.com>
2022-04-26 16:09:56 +0300

    added cloud database and dotenv and env configs

--- server.js
+++ server.js
@@ -17,13 +17,19 @@ github > comment code  wip
 */
 
 //tier 1
+const dotenv = require("dotenv");
 const express = require("express");
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/sportApi");
 const routes = require("./routes");
 const app = express();
 
 //middlewares
+dotenv.config({ path: "./config.env" });
+
+const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.DATABASE_PASSWORD);
+
+mongoose.connect(DB).then(() => console.log("DB connection successful!"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -34,6 +40,6 @@ app.post("/api/add", routes.addSport);
 app.put("/api/update/:id", routes.updateById);
 app.delete("/api/delete/:id", routes.deleteById);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("hello from port 3000");
 });
