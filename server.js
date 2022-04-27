15df279d2917fdbf12816348cb15eb31987eccbb
kouzber123 <48252892+kouzber123@users.noreply.github.com>
2022-04-27 13:08:39 +0300

    trying

--- server.js
+++ server.js
@@ -34,13 +34,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //routes
-app.get("/*", routes.generalMessage);
 app.get("/api/getall", routes.getAllSports);
 app.get("/api/:id", routes.getByID);
 app.post("/api/add", routes.addSport);
 app.put("/api/update/:id", routes.updateById);
 app.delete("/api/delete/:id", routes.deleteById);
-
+app.get("*", routes.generalMessage);
 const port = process.env.PORT;
 app.listen(port || 3000, () => {
   console.log("hello 🤣 from port " + port);
